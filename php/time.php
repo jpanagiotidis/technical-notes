b25634fd76a932fdc301767aa379a47acbb9ac08
jpanagiotidis-desktop <panagiotidisjonathan@gmail.com>
2015-01-01 14:53:45 +0200

    added some ubuntu tweaks

---
+++ php/time.php
@@ -0,0 +1,7 @@
+
+	$timezone = new DateTimeZone($variables['field_twfq_event_date'][0]['timezone_db']);
+  $timezoneATH = new DateTimeZone($variables['field_twfq_event_date'][0]['timezone']);
+  $dateObj = (new DateTime($variables['field_twfq_event_date'][0]['value'], $timezone));
+  $dateObj->setTimezone($timezoneATH);
+  print $dateObj->format('d.m.Y');
+  print $dateObj->format('Y/m/d H:i');
