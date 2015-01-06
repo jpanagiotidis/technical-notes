0ed3715c2bd9e4a1a2161d914a7fd4a208784cd0
jpanagiotidis-desktop <panagiotidisjonathan@gmail.com>
2015-01-06 15:35:34 +0200

    added some more drupal and php notes

--- php/time.php
+++ php/time.php
@@ -1,7 +1,10 @@
-
+## Create a date object and change its timezone
 	$timezone = new DateTimeZone($variables['field_twfq_event_date'][0]['timezone_db']);
   $timezoneATH = new DateTimeZone($variables['field_twfq_event_date'][0]['timezone']);
   $dateObj = (new DateTime($variables['field_twfq_event_date'][0]['value'], $timezone));
   $dateObj->setTimezone($timezoneATH);
   print $dateObj->format('d.m.Y');
   print $dateObj->format('Y/m/d H:i');
+
+## Set the timestamp
+  $dateObj->setTimestamp($node->created);
